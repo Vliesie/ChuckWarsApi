1851befc48ba01acb1b9778d738d7b0a40ca53d6
Roy <janrobotes@gmail.com>
2022-08-14 19:42:27 +0200

    Update

--- ChuckWarsAPI/Program.cs
+++ ChuckWarsAPI/Program.cs
@@ -8,32 +8,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Key Auth", Version = "v1" });
-//    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
-//    {
-//        Description = "ApiKey must appear in header",
-//        Type = SecuritySchemeType.ApiKey,
-//        Name = "XApiKey",
-//        In = ParameterLocation.Header,
-//        Scheme = "ApiKeyScheme"
-//    });
-//    var key = new OpenApiSecurityScheme()
-//    {
-//        Reference = new OpenApiReference
-//        {
-//            Type = ReferenceType.SecurityScheme,
-//            Id = "ApiKey"
-//        },
-//        In = ParameterLocation.Header
-//    };
-//    var requirement = new OpenApiSecurityRequirement
-//                    {
-//                             { key, new List<string>() }
-//                    };
-//    c.AddSecurityRequirement(requirement);
-//});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Key Auth", Version = "v1" });
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "ApiKey must appear in header",
+        Type = SecuritySchemeType.ApiKey,
+        Name = "XApiKey",
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+    var key = new OpenApiSecurityScheme()
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+    var requirement = new OpenApiSecurityRequirement
+                    {
+                             { key, new List<string>() }
+                    };
+    c.AddSecurityRequirement(requirement);
+});
 
 // Add services to the container.
 builder.Services.AddDbContext<ApiContext>
