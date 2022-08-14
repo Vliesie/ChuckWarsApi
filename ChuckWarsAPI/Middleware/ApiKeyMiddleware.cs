050870f48e825cb3236a9b462f98ca3e4fe43017
Roy <janrobotes@gmail.com>
2022-08-14 19:28:38 +0200

    Testing Swagger UI without Oauth

--- ChuckWarsAPI/Middleware/ApiKeyMiddleware.cs
+++ ChuckWarsAPI/Middleware/ApiKeyMiddleware.cs
@@ -6,31 +6,31 @@ namespace ChuckWarsAPI.Middleware
 {
     public class ApiKeyMiddleware : Exception
     {
-        private readonly RequestDelegate _next;
-        private
-        const string APIKEY = "XApiKey";
-        public ApiKeyMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-        public async Task InvokeAsync(HttpContext context)
-        {
-            if (!context.Request.Headers.TryGetValue(APIKEY, out var extractedApiKey))
+        //private readonly RequestDelegate _next;
+        //private
+        //const string APIKEY = "XApiKey";
+        //public ApiKeyMiddleware(RequestDelegate next)
+        //{
+        //    _next = next;
+        //}
+        //public async Task InvokeAsync(HttpContext context)
+        //{
+        //    if (!context.Request.Headers.TryGetValue(APIKEY, out var extractedApiKey))
 
-            {
-                context.Response.StatusCode = 401;
-                await context.Response.WriteAsJsonAsync("API Key was not provided ");
-                return;
-            }
-            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
-            var apiKey = appSettings.GetValue<string>(APIKEY);
-            if (!apiKey.Equals(extractedApiKey))
-            {
-                context.Response.StatusCode = 401;
-                await context.Response.WriteAsync("Unauthorized client");
-                return;
-            }
-            await _next(context); 
-        }
+        //    {
+        //        context.Response.StatusCode = 401;
+        //        await context.Response.WriteAsJsonAsync("API Key was not provided ");
+        //        return;
+        //    }
+        //    var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+        //    var apiKey = appSettings.GetValue<string>(APIKEY);
+        //    if (!apiKey.Equals(extractedApiKey))
+        //    {
+        //        context.Response.StatusCode = 401;
+        //        await context.Response.WriteAsync("Unauthorized client");
+        //        return;
+        //    }
+        //    await _next(context); 
+        //}
     }
 }
